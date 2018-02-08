606cc0fcde013ef78b5fc84b1cd81ae549f9e0c3
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-02-08 07:34:22 +0100

    pipeline integration
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ src/main/java/com/cloudbees/plugins/codeship/CodeShip.java
@@ -0,0 +1,12 @@
+package com.cloudbees.plugins.codeship;
+
+/**
+ * Convert a CodeShip pipeline (<code>codeship-steps.yaml</code> and al.) into a Jenkins CPS Pipeline script.
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class CodeShip {
+
+    public static String translate(String steps) {
+        return null;
+    }
+}
